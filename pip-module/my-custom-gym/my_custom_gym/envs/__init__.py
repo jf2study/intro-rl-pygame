64e8d116aa9de0d929ccd33914150f5fb9610f10
Jennifer Fong <jenfong@starspiral.com>
2021-06-16 05:06:37 -0700

    PIP module code for Open AI Gym Registry of PyGame reinforcement learning environment

---
+++ pip-module/my-custom-gym/my_custom_gym/envs/__init__.py
@@ -0,0 +1,4 @@
+from my_custom_gym.envs.foo_env import FooEnv
+from my_custom_gym.envs.my_py_game_env import MyPyGameEnv
+from my_custom_gym.envs.my_py_game_env import Player
+from my_custom_gym.envs.my_py_game_env import Enemy
