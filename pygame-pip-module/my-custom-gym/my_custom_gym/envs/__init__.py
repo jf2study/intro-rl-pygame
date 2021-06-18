8bc165c83f5272f6b19ba0e102cb1cc9f9a6a4e6
Jennifer Fong <jenfong@starspiral.com>
2021-06-18 02:25:40 -0700

    Removed unused import

--- pygame-pip-module/my-custom-gym/my_custom_gym/envs/__init__.py
+++ pygame-pip-module/my-custom-gym/my_custom_gym/envs/__init__.py
@@ -1,4 +1,3 @@
-from my_custom_gym.envs.foo_env import FooEnv
 from my_custom_gym.envs.my_py_game_env import MyPyGameEnv
 from my_custom_gym.envs.my_py_game_env import Player
 from my_custom_gym.envs.my_py_game_env import Enemy
